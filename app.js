ae5402744bcac2cbf4a5a1ea516069bc67ad6de7
Thiago <42595307+thiagosemann@users.noreply.github.com>
2024-10-03 10:39:29 -0300

    Colocado o no cache

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const router = require('./router');
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    next();
+  });
+  
 app.use(express.json());
 
 app.use(router);
