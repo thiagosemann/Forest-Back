45c899d1f3230630ba609ba9cb3dbc9f06fadde2
Thiago <tgsemann@gmail.com>
2025-05-16 10:39:36 -0300

    Colocado a comodidades dos predios

--- src/models/Airbnb/predioAirbnbModel.js
+++ src/models/Airbnb/predioAirbnbModel.js
@@ -8,12 +8,65 @@ const getAllPredios = async () => {
 
 // Função para criar um novo prédio
 const createPredio = async (predio) => {
-  const { nome } = predio;
-  const insertPredioQuery = 'INSERT INTO predios (nome) VALUES (?)';
-  const values = [nome];
+  const {
+    nome,
+    piscina,
+    academia,
+    churrasqueira,
+    salao_de_festas,
+    espaco_gourmet,
+    sauna,
+    spa,
+    salao_de_jogos,
+    coworking,
+    jardim_terraco,
+    lavanderia,
+    bicicletario,
+    estacionamento_visitas,
+    elevador_social,
+  } = predio;
+
+  const insertQuery = `
+    INSERT INTO predios (
+      nome,
+      piscina,
+      academia,
+      churrasqueira,
+      salao_de_festas,
+      espaco_gourmet,
+      sauna,
+      spa,
+      salao_de_jogos,
+      coworking,
+      jardim_terraco,
+      lavanderia,
+      bicicletario,
+      estacionamento_visitas,
+      elevador_social
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  const values = [
+    nome,
+    piscina || 0,
+    academia || 0,
+    churrasqueira || 0,
+    salao_de_festas || 0,
+    espaco_gourmet || 0,
+    sauna || 0,
+    spa || 0,
+    salao_de_jogos || 0,
+    coworking || 0,
+    jardim_terraco || 0,
+    lavanderia || 0,
+    bicicletario || 0,
+    estacionamento_visitas || 0,
+    elevador_social || 0,
+  ];
 
   try {
-    const [result] = await connection.execute(insertPredioQuery, values);
+    const [result] = await connection.execute(insertQuery, values);
     return { insertId: result.insertId };
   } catch (error) {
     console.error('Erro ao inserir prédio:', error);
@@ -25,27 +78,73 @@ const createPredio = async (predio) => {
 const getPredioById = async (id) => {
   const query = 'SELECT * FROM predios WHERE id = ?';
   const [predios] = await connection.execute(query, [id]);
-
-  if (predios.length > 0) {
-    return predios[0];
-  } else {
-    return null;
-  }
+  return predios.length > 0 ? predios[0] : null;
 };
 
 // Função para atualizar um prédio
 const updatePredio = async (predio) => {
-  const { id, nome } = predio;
-  const updatePredioQuery = `
-    UPDATE predios 
-    SET nome = ?
+  console.log(predio)
+  const {
+    id,
+    nome,
+    piscina,
+    academia,
+    churrasqueira,
+    salao_de_festas,
+    espaco_gourmet,
+    sauna,
+    spa,
+    salao_de_jogos,
+    coworking,
+    jardim_terraco,
+    lavanderia,
+    bicicletario,
+    estacionamento_visitas,
+    elevador_social,
+  } = predio;
+
+  const updateQuery = `
+    UPDATE predios SET
+      nome = ?,
+      piscina = ?,
+      academia = ?,
+      churrasqueira = ?,
+      salao_de_festas = ?,
+      espaco_gourmet = ?,
+      sauna = ?,
+      spa = ?,
+      salao_de_jogos = ?,
+      coworking = ?,
+      jardim_terraco = ?,
+      lavanderia = ?,
+      bicicletario = ?,
+      estacionamento_visitas = ?,
+      elevador_social = ?
     WHERE id = ?
   `;
-  const values = [nome, id];
+
+  const values = [
+    nome,
+    piscina || 0,
+    academia || 0,
+    churrasqueira || 0,
+    salao_de_festas || 0,
+    espaco_gourmet || 0,
+    sauna || 0,
+    spa || 0,
+    salao_de_jogos || 0,
+    coworking || 0,
+    jardim_terraco || 0,
+    lavanderia || 0,
+    bicicletario || 0,
+    estacionamento_visitas || 0,
+    elevador_social || 0,
+    id,
+  ];
 
   try {
-    const [result] = await connection.execute(updatePredioQuery, values);
-    return result.affectedRows > 0; // Retorna true se o prédio foi atualizado com sucesso
+    const [result] = await connection.execute(updateQuery, values);
+    return result.affectedRows > 0;
   } catch (error) {
     console.error('Erro ao atualizar prédio:', error);
     throw error;
@@ -54,11 +153,11 @@ const updatePredio = async (predio) => {
 
 // Função para deletar um prédio pelo ID
 const deletePredio = async (id) => {
-  const deletePredioQuery = 'DELETE FROM predios WHERE id = ?';
+  const query = 'DELETE FROM predios WHERE id = ?';
 
   try {
-    const [result] = await connection.execute(deletePredioQuery, [id]);
-    return result.affectedRows > 0; // Retorna true se o prédio foi deletado com sucesso
+    const [result] = await connection.execute(query, [id]);
+    return result.affectedRows > 0;
   } catch (error) {
     console.error('Erro ao deletar prédio:', error);
     throw error;
@@ -70,5 +169,5 @@ module.exports = {
   createPredio,
   getPredioById,
   updatePredio,
-  deletePredio
+  deletePredio,
 };
